a2bdba141c2099e7ba0a23cc7e1e3c19b5563fc3
tfiosecmtv <tfiosecmtv@gmail.com>
2024-02-17 15:52:32 -0500

    add comments

--- module.py
+++ module.py
@@ -3,12 +3,16 @@ from string import punctuation
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 
+# read stop words from the project file
 stop_words = []
 f = open('proj1-stop.txt', 'r')
 for word in f:
     stop_words.append(word.strip())
 
 def remove_punctuation(input_string):
+    """
+    Clean the input string by removing punctuation signs.
+    """
     # Create a translation table to remove punctuation
     translation_table = str.maketrans("", "", punctuation)
     # Apply the translation to the input string
@@ -16,7 +20,10 @@ def remove_punctuation(input_string):
     return cleaned_string
 
 def remove_stop_words(relevant_docs):
-  # stop word removal
+  """
+  Remove stop words from the user-marked relevant documents.
+  """
+  # Stop word removal
   removed_sw = []
   for s in relevant_docs:
     words = s.lower().split()
@@ -27,52 +34,57 @@ def remove_stop_words(relevant_docs):
   return removed_sw
 
 def calc_weights(original_query, relevant_docs):
+  """
+  Calculate weights of original query and relevant documents.
+  """
     
   docs_rem_sw = remove_stop_words(relevant_docs)
 
-  # vectorize
+  # Vectorize
   vectorizer = TfidfVectorizer(stop_words=stop_words, token_pattern=r"(?u)\S\S+")
   X = vectorizer.fit_transform(docs_rem_sw)
   query_embedding = vectorizer.transform([original_query]) # no need to relearn
-  # vectorizer.get_feature_names_out()
 
-  # get cosine similarities for relevant_docs
+  # Get cosine similarities for relevant_docs
   cosine_similarities = query_embedding.dot(X.T).toarray()[0]
   highest_similarity_index = cosine_similarities.argmax()
   # Get the similarity score
-  most_similar_score = cosine_similarities.max()
-
-  # debugging
-  # print(f"Most similar document: {snippets[highest_similarity_index]}")
-  # print(f"Cosine similarity score: {most_similar_score:.4f}") # get the second most similar
-
   most_similar = docs_rem_sw[highest_similarity_index].split()
   query_list = original_query.split()
-  dict_of_weights = {} # sort by sum of weights
+  # Extract the features and sort by sum of weights
+  dict_of_weights = {}
   feature_names = vectorizer.get_feature_names_out()
   dense_tfidf_matrix = X.toarray()
   df_tfidf = pd.DataFrame(dense_tfidf_matrix, columns=feature_names)
   for word in most_similar:
+    # Avoid the word which is already in the query
     if word in query_list:
       continue
     if word in df_tfidf:
-      # print(word, df_tfidf[word].sum())
       dict_of_weights[word] = df_tfidf[word].sum()
   return dict_of_weights
 
 def extract_new_words(original_query, relevant_docs):
+  """
+  Extract two new words for the new query.
+  """
+  # Extract new words from most similar document with weights
   dict_of_weights = calc_weights(original_query, relevant_docs)
 
+  # Get the word with the highest sum of weights
   unique_values = set(dict_of_weights.values())
   max_value = max(unique_values)
   unique_values.remove(max_value)
+  # Second word with highest weight in case weight values are not equal
   second_max_value = max(unique_values) if unique_values else None
 
   words_with_max_weight = []
+  # Match the maximum weight value to get the word from dictionary
   for k,v in dict_of_weights.items():
     if v == max_value:
       words_with_max_weight.append(k)
   words_with_second_max_weight = []
+  # In the event if the relevant document only has one word
   if len(words_with_max_weight) == 1:
     if second_max_value is not None:
       for k,v in dict_of_weights.items():
@@ -82,6 +94,6 @@ def extract_new_words(original_query, relevant_docs):
       return words_with_max_weight
     sorted_list = sorted(words_with_second_max_weight)
     return [words_with_max_weight[0], sorted_list[-1]]
-
+  # Return two new words to append to the original query
   sorted_list = sorted(words_with_max_weight)
   return [sorted_list[-1], sorted_list[-2]]
